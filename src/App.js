2badcf0b95b1b26c85c776b81bb926fbcae18795
Overlander <anuragnandal@hotmail.com>
2022-12-23 09:35:11 -0500

    Display wallpaper and character images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Game from './components/Game';
 
 function App() {
   return (
     <div className="App">
+      <Game />
     </div>
   );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,20 @@
+import beachWallpaper from "../images/waldo_beach.jpg";
+import waldo from "../images/waldo.jpg";
+import wizard from "../images/wizard.jpg";
+import odlaw from "../images/odlaw.jpg";
+import wilma from "../images/wilma.jpg";
+
 const Game = () => {
 
-
   return (
-    <div>
-
+    <div className="game">
+      <div className="characters">
+        <img src={waldo}></img>
+        <img src={wizard}></img>
+        <img src={odlaw}></img>
+        <img src={wilma}></img>
+      </div>
+      <img src={beachWallpaper} className="wallpaper"></img>
     </div>
   )
 }
