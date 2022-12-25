c70e26b340872b0abede20eb1487d1423cf97f36
Overlander <anuragnandal@hotmail.com>
2022-12-25 12:44:30 -0500

    Make nav banner text into a link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return(
     <nav>
-      <h2>Where's Waldo</h2>
+      <Link className="nav-link" to="/">
+        <h2>Where's Waldo</h2>
+      </Link>
       <ul>
         <Link className="nav-link" to="/">
           <li>Home</li>
