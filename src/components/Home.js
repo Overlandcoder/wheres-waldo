24db6ef4dea974c19c0b765ceceba0f401aaf6ca
Overlander <anuragnandal@hotmail.com>
2022-12-27 12:44:02 -0500

    Format code and add image alt attribute

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 import beachWallpaper from "../images/waldo_beach.jpg";
 
 const Home = () => {
-  return(
+  return (
     <div className="home">
       <h1 className="home-heading">Choose a map</h1>
       <Link to="/beach">
-        <img src={beachWallpaper} className="beach-link"></img>
+        <img src={beachWallpaper} className="beach-link" alt="where's waldo beach map"></img>
       </Link>
     </div>
   )
