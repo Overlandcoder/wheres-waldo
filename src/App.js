2aee91c32d858a3ba31a4edd677ab2243b06f822
Overlander <anuragnandal@hotmail.com>
2022-12-31 08:00:13 -0500

    Failed attempt to use local storage to persist current map data

--- src/App.js
+++ src/App.js
@@ -5,21 +5,34 @@ import Nav from "./components/Nav";
 import Home from "./components/Home";
 import beachImage from "./images/beach_image.jpg";
 import skiImage from "./images/ski_image.jpg";
+import spaceImage from "./images/space_image.jpg";
 import Leaderboard from "./components/Leaderboard";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const maps = [{ "name": "beach", "image": beachImage },
-                { "name": "ski", "image": skiImage }];
+                { "name": "ski", "image": skiImage },
+                { "name": "space", "image": spaceImage }];
 
   const [currentMap, setCurrentMap] = useState(maps[0]);
+  const handleClick = map => setCurrentMap(map);
   const formattedTime = seconds => {
     return `${Math.floor(seconds / 60)}:${String(seconds % 60).padStart(2, '0')}`;
   }
 
-  const handleClick = map=> {
-    setCurrentMap(map);
-  }
+  useEffect(() => {
+    const data = window.localStorage.getItem("currentMapName");
+    const findMapByName = maps.find(map => map.name === data);
+    console.log(`data: ${data}`)
+    console.log(findMapByName)
+    console.log("current")
+    console.log(currentMap.name)
+    if ( currentMap === undefined ) setCurrentMap(findMapByName);
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("currentMapName", currentMap.name);
+  }, [currentMap.name]);
 
   return (
     <Router>
