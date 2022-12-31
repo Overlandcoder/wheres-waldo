a41c7ad3f7819860fa99a5ceec3a616c89e69bb9
Overlander <anuragnandal@hotmail.com>
2022-12-31 10:55:15 -0500

    Fix nav links routes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return(
     <nav>
-      <Link className="nav-link nav-heading" to="/">
+      <Link className="nav-link nav-heading" to="/wheres_waldo">
         Where's Waldo
       </Link>
       <ul>
-        <Link className="nav-link" to="/">
+        <Link className="nav-link" to="/wheres_waldo">
           <li>Home</li>
         </Link>
-        <Link className="nav-link" to="/leaderboard">
+        <Link className="nav-link" to="/wheres_waldo/leaderboard">
           <li>Leaderboard</li>
         </Link>
       </ul>
