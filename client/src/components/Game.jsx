42c0178e0ee39b1a502ea493f282f2d5d14316f7
Overlander <anuragnandal@hotmail.com>
2026-03-03 17:35:58 -0500

    feat: implement click coordinate tracking

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -1,9 +1,27 @@
+import "./Game.css"
 import { useRef, useState } from "react"
 
-function Game({mapName, imageUrl}) {
-  return(
+function Game({ mapName, imageUrl }) {
+  const imageRef = useRef(null);
+
+  const handleClick = (event) => {
+    const rect = imageRef.current.getBoundingClientRect();
+    const xPixel = event.clientX - rect.left;
+    const yPixel = event.clientY - rect.top;
+    const xPercent = (xPixel / rect.width) * 100;
+    const yPercent = (yPixel / rect.height) * 100;
+    console.log(xPercent, yPercent)
+  }
+
+  return (
     <div className="game-container">
-      <img src={imageUrl} alt={mapName}></img>
+      <img
+        ref={imageRef}
+        src={imageUrl}
+        className="game-image"
+        alt={mapName}
+        onClick={handleClick}
+      />
     </div>
   )
 }
