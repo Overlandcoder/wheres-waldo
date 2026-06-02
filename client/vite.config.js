7bb602a833c1932307fb127425aad9b02592ecd5
Overlander <anuragnandal@hotmail.com>
2026-06-02 14:15:27 -0400

    fix: use dynamic vite base path for dev & prod

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/wheres-waldo/",
-});
+  base: command === "build" ? "/wheres-waldo/" : "/",
+}));
