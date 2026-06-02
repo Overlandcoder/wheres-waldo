2a4e59a9db38f144a8f65f5f531de4e4af005966
Overlander <anuragnandal@hotmail.com>
2026-06-02 14:56:13 -0400

    fix: use vite base url for dynamic beach image pathing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from "react"
-import "./App.css"
-import Game from "./components/Game"
+import "./App.css";
+import Game from "./components/Game";
 
 function App() {
   return (
     <div className="App">
       <h1>Where's Waldo?</h1>
-      <Game mapName="Beach" imageUrl="/beach.jpg" />
+      <Game mapName="Beach" imageUrl={`${import.meta.env.BASE_URL}beach.jpg`} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
